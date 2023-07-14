89d91c7285a3c2ee9201081754fab7505ff5c1a8
Strahinja Keselj <shinkansen@pop-os.localdomain>
2023-07-14 16:18:15 +0200

    added nav and stuff

--- src/routes/api/auth/callback/+server.ts
+++ src/routes/api/auth/callback/+server.ts
@@ -1,15 +1,17 @@
-import { error, redirect, type RequestHandler } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
 import { SPOTIFY_APP_CLIENT_ID, SPOTIFY_APP_CLIENT_SECRET, BASE_URL } from '$env/static/private';
 
 export const GET: RequestHandler = async ({ url, cookies, fetch }) => {
+	console.log('im here in callback')
 	const code = url.searchParams.get('code') || null;
 	const state = url.searchParams.get('state') || null;
 
 	const storedState = cookies.get('spotify_auth_state') || null;
-	const storedChallengeVerifier = cookies.get('spotify_auth_challenge_verifier');
+	const storedChallengeVerifier = cookies.get('spotify_auth_challenge_verifier') || null;
 
 	if (state === null || state !== storedState) {
-		throw error(400, 'state missmatch!');
+		throw error(400, 'State Mismatch!');
 	}
 
 	const response = await fetch('https://accounts.spotify.com/api/token', {
@@ -33,9 +35,9 @@ export const GET: RequestHandler = async ({ url, cookies, fetch }) => {
 	if (responseJSON.error) {
 		throw error(400, responseJSON.error_description);
 	}
+
 	cookies.delete('spotify_auth_state');
 	cookies.delete('spotify_auth_challenge_verifier');
-
 	cookies.set('refresh_token', responseJSON.refresh_token, { path: '/' });
 	cookies.set('access_token', responseJSON.access_token, { path: '/' });
 
--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -1,6 +1,7 @@
-import { redirect, type RequestHandler } from '@sveltejs/kit';
-import { SPOTIFY_APP_CLIENT_ID, BASE_URL } from '$env/static/private';
+import { redirect } from '@sveltejs/kit';
 import pkce from 'pkce-gen';
+import type { RequestHandler } from './$types';
+import { SPOTIFY_APP_CLIENT_ID, BASE_URL } from '$env/static/private';
 
 const generateRandomString = (length: number) => {
 	let randomString = '';
@@ -13,13 +14,13 @@ const generateRandomString = (length: number) => {
 };
 
 const state = generateRandomString(16);
-
 const challenge = pkce.create();
 
 const scope =
 	'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read';
 
 export const GET: RequestHandler = ({ cookies }) => {
+
 	cookies.set('spotify_auth_state', state);
 	cookies.set('spotify_auth_challenge_verifier', challenge.code_verifier);
 	throw redirect(
